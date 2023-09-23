2ffd948e4df135b6236b00bdbdd44ba195ebf115
SekkyDSL <127820924SekkyDSL@users.noreply.github.com>
2023-09-23 18:49:40 +0000

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { Loading } from '@/lib/component/Loading';
+import { Suspense } from 'react';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap'
+});
+
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='ja'className={inter.className}>
+      <body>
+        <header
+        className={`
+        h-16
+        ba-transparent
+        backdrop-blur-md
+        flex
+        fixed
+        w-full
+        px-6
+        `}>
+          <div
+          className={`
+          h-auto
+          my-auto
+          font-bold
+          text-5xl
+          tracking-tighter
+          `}>
+            Search Image
+          </div>
+        </header>
+        <main
+        className='pt-20 pb-8 bg-gray-950 min-h-screen'>
+          <Suspense fallback={<Loading />}>
+            {children}
+          </Suspense>
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Search } from "@/lib/component/Search";
+import { getRamdomPhotos } from "@/lib/unsplash";
+
+
+const Home = async () => {
+  const randomPhotos = await getRamdomPhotos();
+  return (
+    <div>
+      <Search randomPhotos={randomPhotos}></Search>
+    </div>
+  );
+};
+
+export default Home
